8a62c24846dc18cd621b3512456a367274858320
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-08-04 15:19:09 -0300

    initial commit

--- UIPickerVIew/ViewController.swift
+++ UIPickerVIew/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()        
     }
+    
+    // MARK: - Picker DataSource and Delegate
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        10
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+           return String(row)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
 
+        let myView = UIView(frame: CGRect(x: 0, y: 0, width: 48, height: 48))
 
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 48, height: 48))
+
+        imageView.image = UIImage(named:"image1")
+        
+        myView.addSubview(imageView)
+
+        return myView
+    }
 }
 
